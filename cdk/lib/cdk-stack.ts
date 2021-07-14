a418c98f75d1a1709d54d58f62a3677c601c6da8
Mark Melia <mark.melia@infinityworks.com>
2021-07-14 14:44:16 +0100

    cloudfront distribution added

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -2,6 +2,8 @@ import * as cdk from '@aws-cdk/core';
 import * as iam from '@aws-cdk/aws-iam';
 import * as s3 from '@aws-cdk/aws-s3';
 import * as s3Deployment from '@aws-cdk/aws-s3-deployment';
+import * as cloudfront from '@aws-cdk/aws-cloudfront';
+import * as origins from '@aws-cdk/aws-cloudfront-origins';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -20,7 +22,10 @@ export class CdkStack extends cdk.Stack {
       destinationBucket: bucket,
       sources: [s3Deployment.Source.asset("../build")]
     })
-
+    
+    const distribution = new cloudfront.Distribution(this, 'mtm-static-react-app-distribution', {
+      defaultBehavior: {origin: new origins.S3Origin(bucket) }
+    })
     //3. Add permission boundary
     const boundary = iam.ManagedPolicy.fromManagedPolicyArn(
       this,
@@ -34,6 +39,11 @@ export class CdkStack extends cdk.Stack {
     new cdk.CfnOutput(this, "Bucket URL", {
       value: bucket.bucketDomainName,
     })
+    new cdk.CfnOutput(this, "Cloudfront URL", {
+      value: distribution.distributionDomainName,
+    })
+
+    //5. Cloudfront
 
   }
 }
